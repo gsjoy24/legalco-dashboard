fcbf38ded7c61bac0598638d157c69c29883ddb4
jmjubaer <jmjubaer3927@gmail.com>
2023-10-15 10:39:48 +0600

    crate add lawyer route

--- src/app/api/lawyers/route.js
+++ src/app/api/lawyers/route.js
@@ -12,3 +12,16 @@ export const GET = async () => {
 		NextResponse.json({ error: 'error for getting data' });
 	}
 };
+
+export const POST = async (request) => {
+	try {
+		const body = await request.json();
+		const db = await DbConnect();
+		const lawyerCollection = db.collection('lawyer');
+		const result = await lawyerCollection.insertOne({...body});
+		return NextResponse.json(result);
+	} catch (error) {
+		console.error('error for getting data', error);
+		NextResponse.json({ error: 'error for getting data' });
+	}
+};
